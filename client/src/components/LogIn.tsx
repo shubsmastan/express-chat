aeade9d88aad6b3ab129e6ac0a39745ec0972b5b
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-06-11 16:48:57 +0100

    Connect server to MongoDB

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function LogIn() {
+  const [isCreatingAccount, setIsCreatingAccount] = useState(true);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -29,8 +30,25 @@ export default function LogIn() {
             className="block rounded-md pl-1 h-8"
           />
           <button className="text-white bg-sky-700 text-lg rounded-md py-1">
-            Log In
+            {isCreatingAccount ? "Sign up" : "Log in"}
           </button>
+          {isCreatingAccount ? (
+            <p>
+              Have an account?{" "}
+              <a href="#" className="text-sky-700 underline">
+                Sign in
+              </a>
+              .
+            </p>
+          ) : (
+            <p>
+              Don't have an account?{" "}
+              <a href="#" className="text-sky-700 underline">
+                Create one
+              </a>
+              .
+            </p>
+          )}
         </form>
       </div>
     </>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,22 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config(path.resolve(__dirname, "../.env"));
+
+mongoose.set("strictQuery", false);
+(async function () {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+const PORT: string | number = process.env.PORT || 3030;
 
 const app = express();
 
-const PORT: string | number = process.env.PORT || 3030;
+app.post("/signup", (req: Request, res: Response) => {});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
