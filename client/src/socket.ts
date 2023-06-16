5ad1d6a5024db068babcda5c3e3a1a4f222114f2
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-06-16 22:42:24 +0100

    Correct env variable name

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.BASE_URL;
+const URL = import.meta.env.VITE_SERVER_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
